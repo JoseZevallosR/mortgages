52be118cbe2851cbd9f9a2211b518d39815caab3
jose_zevallos <ingjosezr@gmail.com>
2022-11-14 16:21:06 -0500

    checking editable views with js

--- mortages/editable/urls.py
+++ mortages/editable/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from editable.views import MyView
+from editable.views import MyView, MyEdit
 from . import views
 
 
 urlpatterns = [
 	path('',views.home, name='home'),
 	path('test/',MyView.as_view(), name='test'),
+	path('edit/',MyEdit.as_view(), name='test'),
 ]
--- mortages/editable/views.py
+++ mortages/editable/views.py
@@ -7,6 +7,10 @@ import pandas as pd
 # Create your views here.
 #https://docs.djangoproject.com/en/4.1/topics/class-based-views/intro/
 
+#https://stackoverflow.com/questions/72561111/send-post-from-js-to-django
+#https://ridwanray.medium.com/django-and-fetch-api-form-submissions-without-page-reloading-dc5106598005
+#how to sent a json object with javascript back to django post method
+
 def home(request):
 	return render(request,'home.html')
 
@@ -25,7 +29,7 @@ class MyView(View):
 		ctx={'form': form}
 		if form.is_valid():
 			bc_data = request.FILES['file_upload']
-			tables = pd.read_excel(bc_data,sheet_name='tdis').to_html(classes=["table", "table-hover"],justify='center',index=False)#, justify='center'
+			tables = pd.read_excel(bc_data,sheet_name='tdis').to_html(classes=["table", "table-hover"],justify='center',index=False,table_id='time_dis')#, justify='center'
 			tables = tables.replace('<td>', '<td class="editable" align="center">')
 			ctx['tables'] = tables
 			return render(request,self.template_name,ctx)
@@ -33,3 +37,22 @@ class MyView(View):
 		return render(request, self.template_name, ctx)
 
 
+class MyEdit(View):
+	
+	template_name = 'editable/edit_table.html'
+
+	def get(self,request,*args, **kwargs):
+		bc_data = '/home/wk-gida1/Documentos/Jose/mortgages/mortages/Transient_boundary_conditions.xlsx'
+		ctx={}
+		tables = pd.read_excel(bc_data,sheet_name='tdis').to_html(classes=["table", "table-hover"],justify='center',index=False,table_id='time_dis')#, justify='center'
+		tables = tables.replace('<td>', '<td class="editable" align="center">')
+		ctx['tables'] = tables
+
+		return render(request,self.template_name,ctx)
+
+	def post(self,request,*args, **kwargs):
+		
+		ctx = {}
+		print(request.POST['df'])
+		
+		return render(request, self.template_name, ctx)
